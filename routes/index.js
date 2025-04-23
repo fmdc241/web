1e8c2600915dda0bd798ba35625b6aea7688bba5
fmdc241 <hexus7101@gmail.com>
2025-04-23 11:55:14 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
+// routes/index.js
 const express = require('express');
 const router = express.Router();
+const { protect, admin } = require('../middleware/authMiddleware');
 
+// Public routes
 router.use('/auth', require('./authRoutes'));
-router.use('/users', require('./userRoutes'));
-router.use('/exams', require('./examRoutes'));
-router.use('/toc', require('./tocRoutes'));
-router.use('/youtube', require('./youtubeRoutes'));
+
+// Protected routes
+router.use('/users', protect, require('./userRoutes'));
+router.use('/exams', protect, require('./examRoutes'));
+router.use('/toc', protect, require('./tocRoutes'));
+router.use('/youtube', protect, require('./youtubeRoutes'));
+
+// Admin-only routes
+// router.use('/admin', protect, admin, require('./adminRoutes'));
 
 module.exports = router;
